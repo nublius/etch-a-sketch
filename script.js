42c92498a6c9397fc85f53b2a7095bd929e7800a
nublius <184036331+nublius@users.noreply.github.com>
2025-03-27 19:19:40 -0700

    Finish project

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const mainContainer = document.getElementById("main-container");
-const rows = 16;
-const columns = 16;
+let rows = 16;
+let columns = 16;
 
+// INITIALIZE grid
 const makeGrid = function() {
     for (let i = 0; i < columns; i++) {
         const boxColumn = document.createElement("div");
@@ -13,14 +14,33 @@ const makeGrid = function() {
             boxDiv.classList.add("box");
         }
     }
+
+    const boxes = document.querySelectorAll(".box");
+
+    // HOVER function
+    boxes.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.borderColor = "red";
+        })
+    })
 }
 
+// CALL grid function
 makeGrid()
 
-const boxes = document.querySelectorAll(".box");
+const askUser = function() {
+    let count = prompt("Enter dimensions (Maximum is 100)");
+    if (count > 100) {
+        alert("Error, maximum is 100");
+    } else if (count < 1) {
+        alert("Error, 1-100");
+    } else {
+        rows = parseInt(count);
+        columns = parseInt(count);
+        mainContainer.replaceChildren();
+        makeGrid();
+    }
+}
 
-boxes.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.borderColor = "red";
-    })
-})
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", askUser);
